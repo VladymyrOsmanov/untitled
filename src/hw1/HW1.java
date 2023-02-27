924c1a172578fcdde56e3e89b2b7a88da2fe1ed5
VladymyrOsmanov <vladymyr.osmanov@gmail.com>
2023-02-27 11:56:15 -0600

    HW1

--- src/hw1/HW1.java
+++ src/hw1/HW1.java
@@ -2,6 +2,8 @@ package hw1;
 
 public class HW1 {
     public static void main(String[] args) {
+        System.out.println("Hello World");
+
         double a =(-3*5);
         double b =(a/8);
         double c =(20 + b);
